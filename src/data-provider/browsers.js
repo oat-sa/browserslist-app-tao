3bdc100523950b55a33609a3acf275f337224df4
Martin Nicholson <martin@taotesting.com>
2025-05-22 09:29:06 +0200

    refactor: make Safari/iOS display minor versions again

--- src/data-provider/browsers.js
+++ src/data-provider/browsers.js
@@ -56,10 +56,12 @@ function getBrowserListing () {
     }, {});
     for(let device in listing){
         for(let values of Object.values(listing[device])) {
+            // Commented out while we'll be supporting from 17.5 to 17.x:
+            //
             // Safari returns every minor version; discard them and just keep major
-            if (values.browser === 'Safari') {
-                values.versions = Array.from(new Set(values.versions.map(safariVer => parseInt(safariVer, 10))));
-            }
+            // if (values.browser === 'Safari') {
+            //     values.versions = Array.from(new Set(values.versions.map(safariVer => parseInt(safariVer, 10))));
+            // }
             values.versions.sort();
         }
         Object.keys(listing[device]).sort().reduce(
